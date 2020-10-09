b6b3707874e33cc82b28be31a658fd179755ad4a
Bryan Jimenez <bryan@roguedevs.io>
2020-10-08 18:24:29 -0700

    Initial Commit

---
+++ twitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  twitter
+//
+//  Created by Bryan Jimenez on 10/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
